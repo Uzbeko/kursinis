647a4a1f33e04986c76d1772aa4d9661f178a439
Uzbeko <uzbeko@gmail.com>
2017-02-08 21:18:27 +0200

    atskyriau klases BlastXMLparser videuje deklaruotas BlastHit ir BlastHsp klases

--- app/src/main/java/org/biojava/nbio/core/search/io/blast/BlastXMLParser.java
+++ app/src/main/java/org/biojava/nbio/core/search/io/blast/BlastXMLParser.java
@@ -239,16 +239,3 @@ public class BlastXMLParser implements ResultFactory {
 }
 
 
-class BlastHsp extends org.biojava.nbio.core.search.io.Hsp {
-	public BlastHsp(int hspNum, double hspBitScore, int hspScore, double hspEvalue, int hspQueryFrom, int hspQueryTo, int hspHitFrom, int hspHitTo, int hspQueryFrame, int hspHitFrame, int hspIdentity, int hspPositive, int hspGaps, int hspAlignLen, String hspQseq, String hspHseq, String hspIdentityString, Double percentageIdentity, Integer mismatchCount) {
-		super(hspNum, hspBitScore, hspScore, hspEvalue, hspQueryFrom, hspQueryTo, hspHitFrom, hspHitTo, hspQueryFrame, hspHitFrame, hspIdentity, hspPositive, hspGaps, hspAlignLen, hspQseq, hspHseq, hspIdentityString, percentageIdentity, mismatchCount);
-	}
-
-}
-
-class BlastHit extends org.biojava.nbio.core.search.io.Hit {
-	public BlastHit(int hitNum, String hitId, String hitDef, String hitAccession, int hitLen, List<Hsp> hitHsps, Sequence hitSequence) {
-		super(hitNum, hitId, hitDef, hitAccession, hitLen, hitHsps, hitSequence);
-	}
-
-}
