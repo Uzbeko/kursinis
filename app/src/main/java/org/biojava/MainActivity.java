d737b86696d2b5d8dca6c47d166f50f599eeabe0
Uzbeko <uzbeko@gmail.com>
2017-02-12 20:05:33 +0200

    pridedu failus

--- app/src/main/java/org/biojava/MainActivity.java
+++ app/src/main/java/org/biojava/MainActivity.java
@@ -4,7 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import org.biojava.*;
 import org.biojava.demo.DemoSixFrameTranslation;
+import org.biojava.nbio.core.search.io.ResultFactory;
+import org.biojava.nbio.core.search.io.SearchIO;
+import org.biojava.nbio.core.search.io.blast.BlastXMLParser;
 import org.biojava.nbio.core.sequence.DNASequence;
+
+import java.io.File;
+import java.net.URL;
 
 import sun.misc.CompoundEnumeration;
 
@@ -17,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        DemoSixFrameTranslation demo = new DemoSixFrameTranslation();
-//        demo.test();
+        DemoSixFrameTranslation demo = new DemoSixFrameTranslation();
+        demo.test();
 
 //        try {
 //            DNASequence.test();
@@ -26,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
 //            e.printStackTrace();
 //        }
 
-
+////		String resource = "/org/biojava/nbio/core/search/io/blast/testBlastReport.blastxml"; //sena direktorija
+//        String resource = "/core/search/io/blast/testBlastReport.blastxml"; //nauja direktorija
+////		String resource = "/testBlastReport.blastxml";
+//        URL resourceURL = getClass().getResource(resource);
+//        File file = new File(resourceURL.getFile());
+//
+//        ResultFactory blastResultFactory = new BlastXMLParser();
+//        final SearchIO instance;
+//        try {
+//            instance = new SearchIO(file, blastResultFactory);
+//        } catch (Exception e) {
+//            System.out.println("NEsuveike!!!!!!!!!!!!!");
+//        }
     }
+
+
 }
